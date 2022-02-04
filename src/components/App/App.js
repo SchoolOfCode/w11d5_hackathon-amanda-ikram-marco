83d89508c723e579f70ce2a61a47ecee2d8b7063
Marco Tsang <hkmarcoot@gmail.com>
2022-02-04 12:15:41 +0000

    add pokemonviewer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import logo from "./soc-logo.svg";
 import "./App.css";
+import PokemonViewer from "../PokemonViewer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" width="100px" />
         <p>Hackathon Friday</p>
+        <PokemonViewer />
       </header>
     </div>
   );
