20805688ea0713249e1fbb592a94d78f03661c96
ikram <ikranidil@hotmail.co.uk>
2022-02-04 12:31:21 +0000

    added image data of pokemon

--- src/components/PokemonViewer/index.js
+++ src/components/PokemonViewer/index.js
@@ -4,12 +4,14 @@ function PokemonViewer() {
   const { id, data, handleChange } = useFetch(
     `https://pokeapi.co/api/v2/pokemon/`
   );
+  console.log(data)
 
   return (
     <section>
       <h4>Pokemon</h4>
       <input type="number" onChange={handleChange} value={id} />
-      {data && <p>{data.name}</p>}
+      {data && <p>{data.name} </p>}
+      <img src= {data.sprites.front_default} alt= ""></img>
     </section>
   );
 }
